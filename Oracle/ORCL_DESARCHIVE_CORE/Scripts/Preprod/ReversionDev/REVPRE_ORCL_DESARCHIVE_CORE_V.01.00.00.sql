bbc8fd48d7ee9f314bf8ef15891642538ade5553
Javier Augusto Paez Cruz <japaez@itcol.com>
2021-12-01 15:41:40 -0500

    Ajuste general base de datos, SITIO WEB y CORE

--- Oracle/ORCL_DESARCHIVE_CORE/Scripts/Preprod/ReversionDev/REVPRE_ORCL_DESARCHIVE_CORE_V.01.00.00.sql
+++ Oracle/ORCL_DESARCHIVE_CORE/Scripts/Preprod/ReversionDev/REVPRE_ORCL_DESARCHIVE_CORE_V.01.00.00.sql
@@ -1,11 +1,11 @@
 /*
 -------------------------------------------------------------------------------------------------------------------------------
--- OBJETIVO                  : Script de reversion
--- PARÁMETROS DE ENTRADA     :   
+-- OBJETIVO                  : Creacion de objeto para auditoria de instalacion
+-- PARÁMETROS DE ENTRADA     : NA
 -- PARÁMETROS DE SALIDA      : NA   
 -- OBJETOS QUE LO REFERENCIAN: NA
 -- LIDER TÉCNICO             :              
--- FECHAHORA                 : 2021/10/14
+-- FECHAHORA                 : 2021/11/23
 -- REALIZADO POR             : INFORMATICA & TECNOLOGIA (GEDV - JAPC)
 --	                           Este componente fue realizado bajo la metodología de desarrollo de Informática & Tecnología 
 --                             y se encuentra Protegido por las leyes de derechos de autor.
@@ -15,48 +15,67 @@
 -- OBJETIVO MODIFICACIÓN     : 
 -----------------------------------------------------------------------------------------------------------------------------
 */
-ALTER SESSION SET "_ORACLE_SCRIPT"=TRUE;
 DECLARE
-   v_sesion VARCHAR2(4000);
-   EXC_SESION EXCEPTION;
-   CURSOR c_drop 
-   IS
-     SELECT ('ALTER SYSTEM KILL SESSION '||''''||VS.SID||','||VS.SERIAL#||'''')AS SESION_ACTIVA
-     INTO   v_sesion
-     FROM   V$SESSION VS
-     WHERE  USERNAME = 'SCH_ORCL_DESARCHIVE_CORE';
-  r_drop c_drop%ROWTYPE;
-     
+  V_EXISTE_OBJETO  NUMBER;
+  V_CREA_TABLA     VARCHAR2(500);   
 BEGIN
-    OPEN c_drop;
-    LOOP
-       FETCH c_drop INTO r_drop;
-       EXECUTE IMMEDIATE r_drop.sesion_activa;       
-    EXIT WHEN c_drop%NOTFOUND;
-    END LOOP;   
-    CLOSE c_drop;
-EXCEPTION 
-  WHEN EXC_SESION THEN
-     IF c_drop%ISOPEN THEN CLOSE c_drop; END IF;
-     DBMS_OUTPUT.PUT_LINE('Sesion no activa, continua ejecucion scripts....');
+ DBMS_OUTPUT.PUT_LINE('--- CONSULTANDO SI TABLA DE AUDITORIA EXISTE ');
+   BEGIN
+      SELECT 1 EXISTE
+      INTO   V_EXISTE_OBJETO
+      FROM   ALL_TABLES AT
+      WHERE  AT.TABLE_NAME = 'AUDITORIA_OBJETOS';
+   EXCEPTION 
+     WHEN NO_DATA_FOUND THEN
+       V_EXISTE_OBJETO := 0;
+   END;
+   
+   IF V_EXISTE_OBJETO =0 THEN
+       DBMS_OUTPUT.PUT_LINE('--- CREANDO TABLA AUDITORIA OBJETOS ');
+       V_CREA_TABLA := '
+                 CREATE TABLE AUDITORIA_OBJETOS(
+                 ID_AUDITORIA NUMBER(19),
+				 PROYECTO     VARCHAR2(4000),
+				 BD           VARCHAR2(4000),
+                 MENSAJE      VARCHAR2(4000),
+                 COD_ERROR    NUMBER,
+                 ERROR        VARCHAR2(4000),
+                 FECHA        DATE)';
+       EXECUTE IMMEDIATE V_CREA_TABLA;
+	   
+       DBMS_OUTPUT.PUT_LINE('--- CREANDO SECUENCIA');
+       EXECUTE IMMEDIATE 'CREATE SEQUENCE  SEQ_AUDITORIA_OBJETO  MINVALUE 1 INCREMENT BY 1 START WITH 101 CACHE 100 ORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL';
+	   
+       DBMS_OUTPUT.PUT_LINE('--- CREANDO TRIGGER TABLA AUDITORIA OBJETOS ');
+	   EXECUTE IMMEDIATE 'CREATE OR REPLACE TRIGGER TRG_AUDITA_OBJETO BEFORE
+	   INSERT ON AUDITORIA_OBJETOS
+	   FOR EACH ROW
+	   WHEN ( NEW.ID_AUDITORIA IS NULL )
+	   BEGIN
+	     :NEW.ID_AUDITORIA := SEQ_AUDITORIA_OBJETO.NEXTVAL;
+	   END TRG_AUDITA_OBJETO;';
+    END IF;
+    
+    
+    DBMS_OUTPUT.PUT_LINE('--- OTORGANDO PERMISOS A LA TABLA');
+    EXECUTE IMMEDIATE 'GRANT SELECT,INSERT,UPDATE,DELETE ON AUDITORIA_OBJETOS TO SYSTEM';
+EXCEPTION
   WHEN OTHERS THEN
-     IF c_drop%ISOPEN THEN CLOSE c_drop; END IF;
-     DBMS_OUTPUT.PUT_LINE('ERROR:'||SQLCODE||'-'||SQLERRM);
+     DBMS_OUTPUT.PUT_LINE('ERROR: '||SQLCODE||'-'||SQLERRM);
 END;
 /
 
+---- Creacion de procedimiento
+CREATE OR REPLACE PROCEDURE PROC_AUDITA_OBJETOS(V_PROYECTO  VARCHAR2,
+                                                V_BD        VARCHAR2,  
+                                                V_MENSAJE   VARCHAR2,
+                                                V_ERROR     VARCHAR2,
+                                                V_MSJ_ERROR VARCHAR2)
+IS
 BEGIN
-	DBMS_SERVICE.stop_service('ORCL_DESARCHIVE_CORE');
-END;
-/
-BEGIN
-	DBMS_SERVICE.delete_service('ORCL_DESARCHIVE_CORE');
-END;
-/
-
-
-DROP ROLE ROL_DESARCHIVE_CORE;
-DROP USER SCH_ORCL_DESARCHIVE_CORE CASCADE;
-
-DROP TABLESPACE ORCL_DESARCHIVE_CORE_DATA INCLUDING CONTENTS;
-DROP TABLESPACE ORCL_DESARCHIVE_CORE_IDX  INCLUDING CONTENTS;
+  INSERT INTO AUDITORIA_OBJETOS(PROYECTO,BD,MENSAJE, COD_ERROR , ERROR, FECHA)VALUES(V_PROYECTO,V_BD,V_MENSAJE,V_ERROR,V_MSJ_ERROR,SYSDATE);
+  COMMIT;
+EXCEPTION 
+  WHEN OTHERS THEN
+     DBMS_OUTPUT.PUT_LINE('ERROR: '||SQLCODE||'-'||SQLERRM);
+END PROC_AUDITA_OBJETOS;
